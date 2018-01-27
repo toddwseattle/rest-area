bbe655e2ffac185def7495083b9cca9dec167326
Todd Warren <todd@warrenfamily.org>
2018-01-27 12:42:51 -0800

    Add the prettyjson component to build-home

--- src/app/builder/builder.module.ts
+++ src/app/builder/builder.module.ts
@@ -5,11 +5,13 @@ import { BuildHomeComponent } from './build-home/build-home.component';
 import { builderRouter } from './builder.route';
 import { UrlInputComponent } from './url-input/url-input.component';
 import { MapGroupComponent } from './map-group/map-group.component';
+import { PrettyJsonModule } from 'angular2-prettyjson';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    PrettyJsonModule,
     builderRouter
   ],
   declarations: [BuildHomeComponent, UrlInputComponent, MapGroupComponent],
